0418304e529af30459a5f5c97688d3b0cf05fdb8
shrimathiravi07 <shrimathiravi44@gmail.com>
2025-07-20 13:03:54 +0530

    app.py file added

--- app.py
+++ app.py
@@ -1,12 +1,41 @@
-from flask import Flask ,render_template,url_for
+# app.py
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+import pandas as pd
+import os
 
-app=Flask(__name__)
+app = Flask(__name__)
 
+# Load model and preprocessor
+model_path = os.path.join('model', 'model.pkl')
+preprocessor_path = os.path.join('model', 'preprocessor.pkl')
 
-@app.route("/")
-def home():
-    return "welcome to flask file "
+model = pickle.load(open(model_path, 'rb'))
+preprocessor = pickle.load(open(preprocessor_path, 'rb'))
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-if __name__== "__main__" :
+@app.route('/predict', methods=['POST'])
+def predict():
+    input_data = {
+        'area_type': request.form['area_type'],
+        'availability': request.form['availability'],
+        'location': request.form['location'],
+        'size': int(request.form['size']),
+        'total_sqft': float(request.form['total_sqft']),
+        'bath': int(request.form['bath']),
+        'balcony': int(request.form['balcony'])
+    }
+
+    input_df = pd.DataFrame([input_data])
+    processed_input = preprocessor.transform(input_df)
+    prediction = model.predict(processed_input)
+    predicted_price = round(prediction[0], 2)
+
+    return render_template('result.html', price=predicted_price)
+
+if __name__ == '__main__':
     app.run(debug=True)
