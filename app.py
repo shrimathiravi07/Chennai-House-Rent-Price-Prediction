321ec08e8778932e80f2b87ed6c376e6b696f410
shrimathi-07 <shrimathiravi44@gmail.com>
2025-07-20 19:50:43 +0530

    app  file updated

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# app.py
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
@@ -24,7 +23,7 @@ def predict():
         'area_type': request.form['area_type'],
         'availability': request.form['availability'],
         'location': request.form['location'],
-        'size': int(request.form['size']),
+        'size(bhk)': int(request.form['size']),
         'total_sqft': float(request.form['total_sqft']),
         'bath': int(request.form['bath']),
         'balcony': int(request.form['balcony'])
@@ -33,8 +32,9 @@ def predict():
     input_df = pd.DataFrame([input_data])
     processed_input = preprocessor.transform(input_df)
     prediction = model.predict(processed_input)
-    predicted_price = round(prediction[0], 2)
 
+    print("Prediction Output:", prediction)  # Debug print
+    predicted_price = round(prediction[0], 2)
     return render_template('result.html', price=predicted_price)
 
 if __name__ == '__main__':
